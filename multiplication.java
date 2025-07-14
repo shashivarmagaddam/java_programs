380ecd3740d355141a90bb9b60debd151743be81
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-14 18:05:23 +0530

    Create multiplication.java
    multiplication of two numbers by initializing values

---
+++ multiplication.java
@@ -0,0 +1,8 @@
+class multiplication{
+  pubic static void main(String[] args){
+    int a=10,b=10; // assigning values //
+    System.out.println(a*b); // logic for multi of numbers//
+  }
+}
+// github.com/shashivarmagaddam //
+// resultant output would be 100 //
