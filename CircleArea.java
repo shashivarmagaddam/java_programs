841c2d511e1f8892f2399c27bf6679962b5a4640
Shashivarma Gaddam <shashivarmagaddam@gmail.com>
2025-07-15 17:42:36 +0530

    Create CircleArea.java

---
+++ CircleArea.java
@@ -0,0 +1,7 @@
+class CircleArea{
+ public static void main(String[] args){
+ double a=10;
+ double pi=3.14,area=pi*a*a;  // area of circle is pi-r-suare pi*r**2 //
+System.out.println("Area of circle is : "+area);
+}
+}
